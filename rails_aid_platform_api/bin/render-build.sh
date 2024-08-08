aeff95f637864939eeaca9fbd5934696d5ffc0e9
LaTaja Long <taja.long@yahoo.com>
2024-08-08 11:15:57 -0400

    render

--- rails_aid_platform_api/bin/render-build.sh
+++ rails_aid_platform_api/bin/render-build.sh
@@ -2,6 +2,10 @@
 # exit on error
 set -o errexit
 
+# Add build commands for front end
+rm -rf public
+npm install --prefix client && npm run build --prefix client
+cp -a client/build/. public/
+
 bundle install
-bundle exec rails assets:precompile
-bundle exec rails assets:clean
+bundle exec rake db:migrate
