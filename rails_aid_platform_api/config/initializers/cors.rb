e9caf0d526ca6955662e49f284690dd891c2aa5a
LaTaja Long <taja.long@yahoo.com>
2024-08-08 11:51:03 -0400

    heroku

--- rails_aid_platform_api/config/initializers/cors.rb
+++ rails_aid_platform_api/config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:3000"
+    origins "https://aid-platform-8b9c23f8a212.herokuapp.com/"
 
     resource "*",
       headers: :any,
